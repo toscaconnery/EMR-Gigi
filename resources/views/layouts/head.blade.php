25038e6a488219d73b0d7bea37914a90133f71f6
Tosca Yoel Connery <toscayoelconnery@gmail.com>
2020-05-06 16:24:32 +0700

    change the way to access css for heroku

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -9,55 +9,55 @@
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
     <!-- jquery
 		============================================ -->
-    <script src="{{ asset('theme/js/vendor/jquery-1.11.3.min.js') }}"></script>
+    <script src="theme/js/vendor/jquery-1.11.3.min.js"></script>
     <!-- Google Fonts
 		============================================ -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i,800" rel="stylesheet">
     <!-- Bootstrap CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="theme/css/bootstrap.min.css">
     <!-- Bootstrap CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="theme/css/font-awesome.min.css">
     <!-- adminpro icon CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/adminpro-custon-icon.css') }}">
+    <link rel="stylesheet" href="theme/css/adminpro-custon-icon.css">
     <!-- meanmenu icon CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/meanmenu.min.css') }}">
+    <link rel="stylesheet" href="theme/css/meanmenu.min.css">
     <!-- mCustomScrollbar CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/jquery.mCustomScrollbar.min.css') }}">
+    <link rel="stylesheet" href="theme/css/jquery.mCustomScrollbar.min.css">
     <!-- buttons CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/buttons.css') }}">
+    <link rel="stylesheet" href="theme/css/buttons.css">
     <!-- animate CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/animate.css') }}">
+    <link rel="stylesheet" href="theme/css/animate.css">
     <!-- jvectormap CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/jvectormap/jquery-jvectormap-2.0.3.css') }}">
+    <link rel="stylesheet" href="theme/css/jvectormap/jquery-jvectormap-2.0.3.css">
     <!-- normalize CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/data-table/bootstrap-table.css') }}">
-    <link rel="stylesheet" href="{{ asset('theme/css/data-table/bootstrap-editable.css') }}">
+    <link rel="stylesheet" href="theme/css/data-table/bootstrap-table.css">
+    <link rel="stylesheet" href="theme/css/data-table/bootstrap-editable.css">
     <!-- normalize CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/normalize.css') }}">
+    <link rel="stylesheet" href="theme/css/normalize.css">
     <!-- charts CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/c3.min.css') }}">
+    <link rel="stylesheet" href="theme/css/c3.min.css">
     <!-- style CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/style.css') }}">
+    <link rel="stylesheet" href="theme/style.css">
     <!-- responsive CSS
 		============================================ -->
-    <link rel="stylesheet" href="{{ asset('theme/css/responsive.css') }}">
+    <link rel="stylesheet" href="theme/css/responsive.css">
     <!-- modernizr JS
 		============================================ -->
-    <script src="{{ asset('theme/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+    <script src="theme/js/vendor/modernizr-2.8.3.min.js"></script>
     
 
-    <link rel="stylesheet" href="{{ asset('theme/css/datapicker/datepicker3.css') }}">
+    <link rel="stylesheet" href="theme/css/datapicker/datepicker3.css">
   
 </head>
