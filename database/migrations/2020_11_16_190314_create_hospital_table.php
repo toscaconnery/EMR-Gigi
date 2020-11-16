bedb00d5646aa5bc0c875831c4b10df304d8450a
Tosca Yoel Connery <toscayoelconnery@gmail.com>
2020-11-17 02:18:04 +0700

    hospital, branch, and doctor migration

---
+++ database/migrations/2020_11_16_190314_create_hospital_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHospitalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospital', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('address', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hospital');
+    }
+}
---
+++ database/migrations/2020_11_16_190847_create_doctor_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('employee_id', 30)->nullable();
+            $table->enum('gender', ['m', 'f']);
+            $table->string('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor');
+    }
+}
