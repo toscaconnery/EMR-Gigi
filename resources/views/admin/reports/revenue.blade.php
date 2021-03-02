505d20a46d4a59332c8276c51e56e8c47fcdfcd3
Anas aditya <anas.aditya@webimp.com.sg>
2021-03-03 00:20:31 +0700

    update and add dataset chart

--- resources/views/admin/reports/revenue.blade.php
+++ resources/views/admin/reports/revenue.blade.php
@@ -27,6 +27,7 @@
                                     <div class="col-md-3">
                                         <button type="button" class="btn btn-primary">Add Payment Method</button>
                                     </div>
+
                                     <div class="col-md-4">
                                         <p><small>Enter your capital here</small></p>
                                     </div>
@@ -79,26 +80,29 @@
                 labels: ['Jan', 'Feb', 'March', 'April', 'Mai', 'June'],
                 datasets: [{
                     label: 'Revenue',
-                    data: [20, 50, 10, 100, 63, 70],
+                    data: [20, 30, 35, 40, 70, 100],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(8, 144, 248, 0.5)'
                     ],
                     borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(255, 99, 132, 1)'
                     ],
                     borderWidth: 1,
                     hoverBorderWidth: 2,
-                    hoverBorderColor: '#000'
+                    hoverBorderColor: 'tomato'
+                },
+                {
+                    label: 'New Chart',
+                    data: [30, 20, 55, 80, 73, 80],
+                    backgroundColor: [
+                        'rgba(8, 248, 16, 0.5)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)'
+                    ],
+                    borderWidth: 1,
+                    hoverBorderWidth: 2,
+                    hoverBorderColor: 'tomato'
                 }]
             },
             options: {
@@ -110,7 +114,7 @@
                     }]
                 },
                 legend:{
-                    position: 'right'
+                    align: 'end',
                 },
                 layout:{
                     padding:{
