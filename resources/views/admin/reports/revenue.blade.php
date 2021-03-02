92954cdf21579bc9a31974a465d0bf596b28e9d3
Anas aditya <anas.aditya@webimp.com.sg>
2021-03-02 18:05:56 +0700

    create revenue page

--- resources/views/admin/reports/revenue.blade.php
+++ resources/views/admin/reports/revenue.blade.php
@@ -20,13 +20,43 @@
                     <div class="Container col-12">
                         <div class="card">
                             <div class="card-header mb-1">
-                                Admin Dashboard
+                                Report Revenue
                             </div>
                             <div class="card-body">
+                                <div class="row justify-content-between">
+                                    <div class="col-md-3">
+                                        <button type="button" class="btn btn-primary">Add Payment Method</button>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p><small>Enter your capital here</small></p>
+                                    </div>
+                                </div>
+
                                 <div class="container">
-                                    <div class="ava">
-                                        <h1 style="text-align: center">Welcome to admin dashboard</h1>
-                                        <img src="{{getasset('image/logo-square.png')}}" class="mx-auto d-block"  style="width: 150px; height: 150px; border-radius: 50%; margin-right: 25px; margin-bottom: 10px;">
+                                    <div class="row">
+                                        <div class="col-md-2 box">
+                                            <h3>IDR 1000</h3>
+                                            <p>This year</p>
+                                        </div>
+                                        <div class="col-md-2 box">
+                                            <h3>IDR 500</h3>
+                                            <p>This month</p>
+                                        </div>
+                                        <div class="col-md-2 box">
+                                            <h3>IDR 100</h3>
+                                            <p>This week</p>
+                                        </div>
+                                        <div class="col-md-2 box">
+                                            <h3>IDR 80</h3>
+                                            <p>Today</p>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-10">
+                                            <canvas id="myChart" height="40vh" width="80vw"></canvas>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -42,8 +72,52 @@
     @include('admin_layout.footscript')
 
     <script>
-        $(document).ready(function(){
-
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Jan', 'Feb', 'March', 'April', 'Mai', 'June'],
+                datasets: [{
+                    label: 'Revenue',
+                    data: [20, 50, 10, 100, 63, 70],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                    hoverBorderWidth: 2,
+                    hoverBorderColor: '#000'
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                legend:{
+                    position: 'right'
+                },
+                layout:{
+                    padding:{
+                        top:30,
+                    }
+                }
+            }
         });
     </script>
 
--- resources/views/admin_layout/head.blade.php
+++ resources/views/admin_layout/head.blade.php
@@ -4,22 +4,25 @@
     <title>Rise EMR</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!-- Google Fonts
 		============================================ -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i,800" rel="stylesheet">
-    
+
     <script src="https://kit.fontawesome.com/2e53fa6365.js" crossorigin="anonymous"></script>
-   
+
+    <!-- ChartJS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{getasset('admin-theme/css/bootstrap.css')}}">
-    
+
     <!-- Main CSS -->
     <link rel="stylesheet" href="{{getasset('admin-theme/css/admin.css')}}">
 
     <!-- Main JS -->
     <link rel="stylesheet" href="{{getasset('admin-theme/js/admin.js')}}">
-    
+
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
 
@@ -32,5 +35,5 @@
     {{-- Toastr --}}
     <link href="{{getasset('css/toastr.css')}}" rel="stylesheet">
     <script src="{{getasset('js/toastr.js')}}"></script>
-  
+
 </head>
