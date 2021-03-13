8fd1b94eda8a40096240a3fae24a1b6b75005a49
Tosca Yoel Connery <toscayoelconnery@gmail.com>
2021-03-14 03:17:56 +0700

    icon

--- resources/views/admin_layout/head.blade.php
+++ resources/views/admin_layout/head.blade.php
@@ -4,6 +4,7 @@
     <title>Rise EMR</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{getasset('image/emrgigi.ico')}}">
 
     <!-- Google Fonts
 		============================================ -->
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,64 +4,27 @@
     <title>Rise EMR</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- favicon
-		============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
-    <!-- jquery
-		============================================ -->
+    
     <script src="theme/js/vendor/jquery-1.11.3.min.js"></script>
-    <!-- Google Fonts
-		============================================ -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i,800" rel="stylesheet">
-    <!-- Bootstrap CSS
-		============================================ -->
-    <link rel="stylesheet" href="theme/css/bootstrap.min.css">
-    <!-- Bootstrap CSS
-		============================================ -->
-    {{-- <link rel="stylesheet" href="theme/css/font-awesome.min.css"> --}}
     <script src="https://kit.fontawesome.com/2e53fa6365.js" crossorigin="anonymous"></script>
-    <!-- adminpro icon CSS
-		============================================ -->
+    <script src="theme/js/vendor/modernizr-2.8.3.min.js"></script>
+    
+    <link rel="icon" href="{{getasset('image/emrgigi.ico')}}">
+    <link rel="stylesheet" href="theme/css/bootstrap.min.css">
     <link rel="stylesheet" href="theme/css/adminpro-custon-icon.css">
-    <!-- meanmenu icon CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/meanmenu.min.css">
-    <!-- mCustomScrollbar CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/jquery.mCustomScrollbar.min.css">
-    <!-- buttons CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/buttons.css">
-    <!-- animate CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/animate.css">
-    <!-- jvectormap CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/jvectormap/jquery-jvectormap-2.0.3.css">
-    <!-- normalize CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/data-table/bootstrap-table.css">
     <link rel="stylesheet" href="theme/css/data-table/bootstrap-editable.css">
-    <!-- normalize CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/normalize.css">
-    <!-- charts CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/c3.min.css">
-    <!-- style CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/style.css">
-    <!-- responsive CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/responsive.css">
-    <!-- modernizr JS
-		============================================ -->
-    <script src="theme/js/vendor/modernizr-2.8.3.min.js"></script>
-    <!-- notifications CSS
-		============================================ -->
     <link rel="stylesheet" href="theme/css/Lobibox.min.css">
     <link rel="stylesheet" href="theme/css/notifications.css">
-
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i,800" rel="stylesheet">
     <link rel="stylesheet" href="theme/css/datapicker/datepicker3.css">
-  
 </head>
