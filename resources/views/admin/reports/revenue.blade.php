4bd187aeb443ab08bbef88d021fc8d85e5798c12
Anas aditya <anas.aditya@webimp.com.sg>
2021-04-02 15:20:21 +0700

    add input number on revenue

--- resources/views/admin/reports/revenue.blade.php
+++ resources/views/admin/reports/revenue.blade.php
@@ -25,11 +25,37 @@
                             <div class="card-body">
                                 <div class="row justify-content-between">
                                     <div class="col-md-3">
-                                        <button type="button" class="btn btn-primary">Add Payment Method</button>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Add Payment Method</button>
+
+                                          <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                ...
+                                                </div>
+                                                <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                                </div>
+                                            </div>
+                                            </div>
+                                        </div>
                                     </div>
 
-                                    <div class="col-md-4">
-                                        <p><small>Enter your capital here</small></p>
+                                    <div class="col-md-5">
+                                        <div class="form-group row">
+                                            <label for="typeNumber" class="col-form-label"><small>Enter your capital here</small></label>
+                                            <div class="col-sm-6">
+                                                <input type="number" class="form-control" id="typeNumber" placeholder="e.g Rp500.000">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
 
@@ -51,7 +77,6 @@
                                             <h3>IDR 80</h3>
                                             <p>Today</p>
                                         </div>
-
                                     </div>
 
                                     <div class="row">
@@ -73,56 +98,59 @@
     @include('admin_layout.footscript')
 
     <script>
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: ['Jan', 'Feb', 'March', 'April', 'Mai', 'June'],
-                datasets: [{
-                    label: 'Revenue',
-                    data: [20, 30, 35, 40, 70, 100],
-                    backgroundColor: [
-                        'rgba(8, 144, 248, 0.5)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)'
-                    ],
-                    borderWidth: 1,
-                    hoverBorderWidth: 2,
-                    hoverBorderColor: 'tomato'
-                },
-                {
-                    label: 'New Chart',
-                    data: [30, 20, 55, 80, 73, 80],
-                    backgroundColor: [
-                        'rgba(8, 248, 16, 0.5)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)'
-                    ],
-                    borderWidth: 1,
-                    hoverBorderWidth: 2,
-                    hoverBorderColor: 'tomato'
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
+
+
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ['Jan', 'Feb', 'March', 'April', 'Mai', 'June'],
+                    datasets: [{
+                        label: 'Revenue',
+                        data: [20, 30, 35, 40, 70, 100],
+                        backgroundColor: [
+                            'rgba(8, 144, 248, 0.5)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)'
+                        ],
+                        borderWidth: 1,
+                        hoverBorderWidth: 2,
+                        hoverBorderColor: 'tomato'
+                    },
+                    {
+                        label: 'New Chart',
+                        data: [30, 20, 55, 80, 73, 80],
+                        backgroundColor: [
+                            'rgba(8, 248, 16, 0.5)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)'
+                        ],
+                        borderWidth: 1,
+                        hoverBorderWidth: 2,
+                        hoverBorderColor: 'tomato'
                     }]
                 },
-                legend:{
-                    align: 'end',
-                },
-                layout:{
-                    padding:{
-                        top:30,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    legend:{
+                        align: 'end',
+                    },
+                    layout:{
+                        padding:{
+                            top:30,
+                        }
                     }
                 }
-            }
-        });
+            });
+
     </script>
 
 </html>
